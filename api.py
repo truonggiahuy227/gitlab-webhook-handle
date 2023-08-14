18ee619653223979f0b02e9c0de2c32f35097625
trương gia huy <truonggiahuy@lilys-air.fis.vn>
2023-08-14 12:17:21 +0700

    Change code

--- api.py
+++ api.py
@@ -53,7 +53,10 @@ def webhook():
         payload = request.json
         # print(payload['object_kind'])
         event_queue.put_nowait(payload)
-        print(payload['labels'][0])
+        if payload['labels']:
+            print(payload['labels'][0])
+        else:
+            print("Issue status Open")
         eventObject = EventObject(payload['event_type'], payload['user'], payload['project']['id'], payload['project']['name'], payload['project']['web_url'], payload['object_attributes']['state'],
                             payload['object_attributes']['severity'], payload['changes'], payload['assignees'])
         event.append(json.dumps(eventObject.__dict__))
